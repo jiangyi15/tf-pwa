4c55657605dc41e01f91ad871722f5e63d68f33a
jiangy <jiangyi15@mails.ucas.ac.cn>
2019-10-27 13:25:13 +0800

    optimize using iminuit

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from amplitude import AllAmplitude
-
+import time 
+import functools
 
 class Model:
   def __init__(self,res,w_bkg = 0):
@@ -16,6 +17,51 @@ class Model:
     n_mc = mcdata[0].shape[0]
     return -(ln_data - self.w_bkg * ln_bg - (n_data - self.w_bkg*n_bg) * int_mc)
   
+  def nll_gradient(self,data,bg,mcdata,batch):
+    n_data = data[0].shape[0]
+    n_bg = bg[0].shape[0]
+    n_mc = mcdata[0].shape[0]
+    N = len(data)
+    data_warp = [tf.concat([data[i],bg[i]],0) for i in range(N)]
+    data_weight = tf.concat([tf.ones(shape=(n_data,),dtype="float32"),-tf.ones(shape=(n_bg,),dtype="float32")*self.w_bkg],0)
+    sum_w = n_data - self.w_bkg * n_bg
+    nll,g = self.sum_gradient(data_warp,data_weight,batch,func=tf.math.log)
+    s,g2 = self.sum_gradient(mcdata,1/n_mc,batch)
+    for i in range(len(g)):
+      g[i] = -g[i] + sum_w * g2[i]/s
+    return -nll+sum_w*tf.math.log(s),g
+  
+  def sum_gradient(self,data,weight=1.0,batch=1536,func = lambda x:x):
+    data_i = []
+    N = len(data)
+    n_data = data[0].shape[0]
+    n_split = (n_data + batch -1) // batch
+    for i in range(n_split):
+      data_i.append([
+        data[j][i*batch:min(i*batch+batch,n_data)] for j in range(N)
+      ])
+    data_wi = []
+    if isinstance(weight,float):
+      weight = tf.ones(n_data)*weight
+    for i in range(n_split):
+      data_wi.append(weight[i*batch:min(i*batch+batch,n_data)])
+    g = None
+    nll = 0.0
+    n_variables = len(self.Amp.trainable_variables)
+    for i in range(n_split):
+      #print(i,min(i*batch+batch,n_data))
+      with tf.GradientTape() as tape:
+        amp2s = self.Amp(data_i[i])
+        l_a = func(amp2s)
+        p_nll = tf.reduce_sum(data_wi[i] * l_a)
+      nll += p_nll
+      a = tape.gradient(p_nll,self.Amp.trainable_variables)
+      if g is None:
+        g = a
+      else :
+        for j in range(n_variables):
+          g[j] += a[j]
+    return nll,g
 
 param_list = [
   "m_BC","m_BD","m_CD",
@@ -119,17 +165,71 @@ config_list = {"D2_2460"
     }
 }
 
-def train_one_step(model, optimizer, data, bg,mc):
-  with tf.GradientTape() as tape:
-    loss = model.nll(data,bg,mc)
-
-  grads = tape.gradient(loss, model.Amp.trainable_variables)
+def train_one_step(model, optimizer, data, bg,mc,batch=16384):
+  nll,grads = model.nll_gradient(data,bg,mc,batch)
+  print(grads)
+  print(nll)
   optimizer.apply_gradients(zip(grads, model.Amp.trainable_variables))
-  print(loss)
-  return loss
+  #with tf.GradientTape() as tape:
+    #nll = model.nll(data,bg,mc)
+  #g = tape.gradient(nll,model.Amp.trainable_variables)
+  #print(nll,g)
+  return nll
+
+class fcn(object):
+  """
+  provide FCN function and gradient for minuit
+  """
+  def __init__(self,model,data,bg,mc,batch=16384):
+    self.model = model
+    self.data = data
+    self.bg = bg
+    self.mc = mc
+    self.batch = batch
+    self.grads = []
+    self.x = None
+    self.nll = 0.0
+    w_bkg = model.w_bkg
+    n_data = data[0].shape[0]
+    n_bg = bg[0].shape[0]
+    self.alpha = (n_data - w_bkg * n_bg)/(n_data + w_bkg**2 * n_bg)
+  def __call__(self,*x):
+    now = time.time()
+    if (not self.x is None) and self.x == x:
+      return self.nll
+    self.x = x
+    train_vars = self.model.Amp.trainable_variables
+    n_var = len(train_vars)
+    for i in range(n_var):
+      train_vars[i].assign(x[i])
+    nll,g = self.model.nll_gradient(self.data,self.bg,self.mc,self.batch)
+    self.grads = [ self.alpha * i.numpy() for i in g]
+    print("nll:",self.alpha * nll," time :",time.time() - now)
+    return self.alpha * nll
+  
+  @functools.lru_cache()
+  def grad(self,*x):
+    if (not self.x is None) and self.x == x:
+      return self.grads
+    self(*x)
+    return self.grads
+  
+def set_gpu_mem_growth():
+  gpus = tf.config.experimental.list_physical_devices('GPU')
+  if gpus:
+    try:
+      # Currently, memory growth needs to be the same across GPUs
+      for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+      logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+      #print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+      # Memory growth must be set before GPUs have been initialized
+      print(e)
 
 def main():
   import json,time
+  set_gpu_mem_growth()
   a = Model(config_list,0.8)
   data = []
   bg = []
@@ -150,36 +250,50 @@ def main():
       tmp_data = tf.Variable(tmp[i],name=i)
       bg.append(tmp_data)
   #print(data,bg,mcdata)
+  import iminuit 
+  f = fcn(a,data,bg,mcdata,27648)# 1356*18
+  args = {}
+  args_name = []
+  for i in a.Amp.trainable_variables:
+    args[i.name] = i.numpy()
+    args_name.append(i.name)
+    args["error_"+i.name] = 0.1
+  m = iminuit.Minuit(f,forced_parameters=args_name,errordef = 0.5,print_level=2,grad=f.grad,**args)
+  now = time.time()
+  with tf.device('/device:GPU:0'):
+    m.migrad()
+  print(time.time() - now)
+  m.get_param_states()
+  exit()
   data_set = tf.data.Dataset.from_tensor_slices(tuple(data))
-  data_set = data_set.shuffle(10000).batch(800)
+  #data_set = data_set.shuffle(10000).batch(800)
   data_set_it = iter(data_set)
   bg_set = tf.data.Dataset.from_tensor_slices(tuple(bg))
-  bg_set = bg_set.shuffle(10000).batch(340)
+  #bg_set = bg_set.shuffle(10000).batch(340)
   bg_set_it = iter(bg_set)
   mc_set = tf.data.Dataset.from_tensor_slices(tuple(mcdata))
-  mc_set = mc_set.shuffle(10000).batch(2520)
+  #mc_set = mc_set.shuffle(10000).batch(2520)
   mc_set_it = iter(mc_set)
   now = time.time()
   with tf.device('/device:GPU:0'):
     print(a.nll(data,bg,mcdata))#.collect_params())
-  optimizer = tf.keras.optimizers.Adagrad()
+  optimizer = tf.keras.optimizers.Adadelta(1.0)
   for i in range(100):
-    try :
-      data_i = data_set_it.get_next()
-      bg_i = bg_set_it.get_next()
-      mcdata_i = mc_set_it.get_next()
-      train_one_step(a,optimizer,data_i,bg_i,mcdata_i);
-    except:
-      data_set = tf.data.Dataset.from_tensor_slices(tuple(data))
-      data_set = data_set.shuffle(10000).batch(800)
-      data_set_it = iter(data_set)
-      bg_set = tf.data.Dataset.from_tensor_slices(tuple(bg))
-      bg_set = bg_set.shuffle(10000).batch(340)
-      bg_set_it = iter(bg_set)
-      mc_set = tf.data.Dataset.from_tensor_slices(tuple(mcdata))
-      mc_set = mc_set.shuffle(10000).batch(2520)
-      mc_set_it = iter(mc_set)
-      
+    #try :
+      #data_i = data_set_it.get_next()
+      #bg_i = bg_set_it.get_next()
+      #mcdata_i = mc_set_it.get_next()
+    train_one_step(a,optimizer,data,bg,mcdata)
+    #except:
+      #data_set = tf.data.Dataset.from_tensor_slices(tuple(data))
+      ##data_set = data_set.shuffle(10000).batch(800)
+      #data_set_it = iter(data_set)
+      #bg_set = tf.data.Dataset.from_tensor_slices(tuple(bg))
+      ##bg_set = bg_set.shuffle(10000).batch(340)
+      #bg_set_it = iter(bg_set)
+      #mc_set = tf.data.Dataset.from_tensor_slices(tuple(mcdata))
+      ##mc_set = mc_set.shuffle(10000).batch(2520)
+      #mc_set_it = iter(mc_set)
   print(time.time()-now)
   #now = time.time()
   #with tf.device('/device:CPU:0'):
