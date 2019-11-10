f9412c420ca14bc517e5cc95ce22e0b3722b6e94
jiangy <jiangyi15@mails.ucas.ac.cn>
2019-11-10 21:27:27 +0800

    Resonances.json

--- model.py
+++ model.py
@@ -75,95 +75,7 @@ param_list = [
   "phi1","phi2"
 ]
 
-config_list = {"D2_2460"
-    :{
-        "m0":2.4607,
-        "m_min":2.4603,
-        "m_max":2.4611,
-        "g0":0.0475,
-        "g_min":0.0464,
-        "g_max":0.0486,
-        "J":2,
-        "Par":1,
-        "Chain":21
-    },
-    "D2_2460p"
-    :{
-        "m0":2.4654,
-        "m_min":2.4644,
-        "m_max":2.4667,
-        "g0":0.0467,
-        "g_min":0.0455,
-        "g_max":0.0479,
-        "J":2,
-        "Par":1,
-        "Chain":121
-    },
-    "D1_2430"
-    :{
-        "m0":2.427,
-        "m_min":2.387,
-        "m_max":2.467,
-        "g0":0.284,
-        "g_min":0.274,
-        "g_max":0.514,
-        "J":1,
-        "Par":1,
-        "Chain":12
-    },
-    "D1_2430p"
-    :{
-        "m0":2.427,
-        "m_min":2.387,
-        "m_max":2.467,
-        "g0":0.384,
-        "g_min":0.274,
-        "g_max":0.514,
-        "J":1,
-        "Par":1,
-        "Chain":112
-    },
-    "D1_2420"
-    :{
-        "m0":2.4208,
-        "m_min":2.4203,
-        "m_max":2.4213,
-        "g0":0.0317,
-        "g_min":0.0292,
-        "g_max":0.0342,
-        "J":1,
-        "Par":1,
-        "Chain":11
-    },
-    "D1_2420p"
-    :{
-        "m0":2.4232,
-        "m_min":2.4208,
-        "m_max":2.4256,
-        "g0":0.025,
-        "g_min":0.019,
-        "g_max":0.021,
-        "J":1,
-        "Par":1,
-        "Chain":111
-    },
-    "Zc_4025"
-    :{
-        "m0":4.0263,
-        "g0":0.0248,
-        "J":1,
-        "Par":1,
-        "Chain":-1
-    },
-    "Zc_4160"
-    :{
-        "m0":4.1628,
-        "g0":0.0701,
-        "J":1,
-        "Par":1,
-        "Chain":-2
-    }
-}
+
 
 def train_one_step(model, optimizer, data, bg,mc,batch=16384):
   nll,grads = model.nll_gradient(data,bg,mc,batch)
@@ -228,8 +140,10 @@ def set_gpu_mem_growth():
       print(e)
 
 def main():
-  import json,time
+  import json
   set_gpu_mem_growth()
+  with open("Resonances.json") as f:  
+    config_list = json.load(f)
   a = Model(config_list,0.8)
   data = []
   bg = []
@@ -264,41 +178,8 @@ def main():
     m.migrad()
   print(time.time() - now)
   m.get_param_states()
-  exit()
-  data_set = tf.data.Dataset.from_tensor_slices(tuple(data))
-  #data_set = data_set.shuffle(10000).batch(800)
-  data_set_it = iter(data_set)
-  bg_set = tf.data.Dataset.from_tensor_slices(tuple(bg))
-  #bg_set = bg_set.shuffle(10000).batch(340)
-  bg_set_it = iter(bg_set)
-  mc_set = tf.data.Dataset.from_tensor_slices(tuple(mcdata))
-  #mc_set = mc_set.shuffle(10000).batch(2520)
-  mc_set_it = iter(mc_set)
-  now = time.time()
-  with tf.device('/device:GPU:0'):
-    print(a.nll(data,bg,mcdata))#.collect_params())
-  optimizer = tf.keras.optimizers.Adadelta(1.0)
-  for i in range(100):
-    #try :
-      #data_i = data_set_it.get_next()
-      #bg_i = bg_set_it.get_next()
-      #mcdata_i = mc_set_it.get_next()
-    train_one_step(a,optimizer,data,bg,mcdata)
-    #except:
-      #data_set = tf.data.Dataset.from_tensor_slices(tuple(data))
-      ##data_set = data_set.shuffle(10000).batch(800)
-      #data_set_it = iter(data_set)
-      #bg_set = tf.data.Dataset.from_tensor_slices(tuple(bg))
-      ##bg_set = bg_set.shuffle(10000).batch(340)
-      #bg_set_it = iter(bg_set)
-      #mc_set = tf.data.Dataset.from_tensor_slices(tuple(mcdata))
-      ##mc_set = mc_set.shuffle(10000).batch(2520)
-      #mc_set_it = iter(mc_set)
-  print(time.time()-now)
-  #now = time.time()
-  #with tf.device('/device:CPU:0'):
-    #print(a(x))#.collect_params())
-  #print(time.time()-now)
+  m.hesse()
+  m.get_param_states()
   with tf.device('/device:GPU:0'):
     print(a.nll(data,bg,mcdata))#.collect_params())
   print(a.Amp.trainable_variables)
