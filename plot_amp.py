1c196a65d082b0a868d21b84d446a1c4b0af0679
jiangy <jiangyi15@mails.ucas.ac.cn>
2020-01-17 13:27:45 +0800

    change plot clolor

--- plot_amp.py
+++ plot_amp.py
@@ -166,12 +166,39 @@ params_config = {
   
 }
 
+model = 3
+
 for i in range(len(param_list)):
   name = param_list[i]
   if name.startswith("beta"):
     name = "cos" + name
   if name in params_config:
     params_config[name]["idx"] = i
+
+def prepare_data(dtype="float64",model="3"):
+  fname = [["./data/data4600_new.dat","data/Dst0_data4600_new.dat"],
+       ["./data/bg4600_new.dat","data/Dst0_bg4600_new.dat"],
+       ["./data/PHSP4600_new.dat","data/Dst0_PHSP4600_new.dat"]
+  ]
+  tname = ["data","bg","PHSP"]
+  data_np = {}
+  for i in range(len(tname)):
+    if model == "3" :
+      data_np[tname[i]] = cal_ang_file(fname[i][0],dtype)
+    elif model == "4":
+      data_np[tname[i]] = cal_ang_file4(fname[i][0],fname[i][1],dtype)
+  def load_data(name):
+    dat = []
+    tmp = flatten_np_data(data_np[name])
+    for i in param_list:
+      tmp_data = tf.Variable(tmp[i],name=i,dtype=dtype)
+      dat.append(tmp_data)
+    return dat
+  #with tf.device('/device:GPU:0'):
+  data = load_data("data")
+  bg = load_data("bg")
+  mcdata = load_data("PHSP")
+  return data, bg, mcdata
 
 def plot(params_file="final_params.json",res_file="Resonances",res_list=None,pm_combine=True):
   POLAR=True
@@ -189,26 +216,9 @@ def plot(params_file="final_params.json",res_file="Resonances",res_list=None,pm_
     else :
       a.set_params(param)
   pprint(a.get_params())
-  fname = [["data/data4600_new.dat","data/Dst0_data4600_new.dat"],
-       ["data/bg4600_new.dat","data/Dst0_bg4600_new.dat"],
-       ["data/PHSP4600_new.dat","data/Dst0_PHSP4600_new.dat"]
-  ]
-  tname = ["data","bg","PHSP"]
-  data_np = {}
-  for i in range(3):
-    data_np[tname[i]] = cal_ang_file(fname[i][0],dtype)
   
-  def load_data(name):
-    dat = []
-    tmp = flatten_np_data(data_np[name])
-    for i in param_list:
-      tmp_data = tf.Variable(tmp[i],name=i,dtype=dtype)
-      dat.append(tmp_data)
-    return dat
-  data = load_data("data")
+  data, bg, mcdata = prepare_data()
   #data = generate_data(8065,3445,w_bkg,1.1,True)
-  bg = load_data("bg")
-  mcdata = load_data("PHSP")
   data_cache = a.Amp.cache_data(*data)
   bg_cache = a.Amp.cache_data(*bg)
   mcdata_cache = a.Amp.cache_data(*mcdata)
@@ -242,31 +252,44 @@ def plot(params_file="final_params.json",res_file="Resonances",res_list=None,pm_
     fitFrac[name] = a_weight[i].sum()/(n_data - w_bkg*n_bg)
   print("FitFractions:")
   pprint(fitFrac)
+  
+  cmap = plt.get_cmap("jet")
+  N = 3 + len(res_list)
+  colors = [cmap(float(i)/N) for i in range(N)]
+  colors = [
+    "black","red","green","blue","yellow","magenta","cyan","purple","teal","springgreen","azure"
+  ] + colors
+  
   def plot_params(ax,name,bins=None,xrange=None,idx=0,display=None,units="GeV",legend=True):
     fd = lambda x:x
     if name.startswith("cos"):
       fd = lambda x:np.cos(x)
     inter = 2
+    color = iter(colors)
     if display is None: display=name
-    data_hist = ax.hist(fd(data[idx].numpy()),range=xrange,bins=bins,histtype="step",label="data",zorder=99,color="black")
+    data_hist = np.histogram(fd(data[idx].numpy()),range=xrange,bins=bins)
+    #ax.hist(fd(data[idx].numpy()),range=xrange,bins=bins,histtype="step",label="data",zorder=99,color="black")
     data_y ,data_x = data_hist[0:2]
     data_x = (data_x[:-1]+data_x[1:])/2
     data_err = np.sqrt(data_y)
-    ax.errorbar(data_x,data_y,yerr=data_err,fmt="k.",zorder = -2)
-    
-    ax.hist(fd(bg[idx].numpy()),range=xrange,bins=bins,histtype="step",weights=[w_bkg]*n_bg,label="bg",zorder = -1)
-    mc_bg = fd(np.append(bg[idx].numpy(),mcdata[idx].numpy()))
-    mc_bg_w = np.append([w_bkg]*n_bg,total.numpy()*norm_int)
+    ax.errorbar(data_x,data_y,yerr=data_err,fmt=".",color=next(color),zorder = -2)
+    if bg is not None:
+      ax.hist(fd(bg[idx].numpy()),range=xrange,bins=bins,histtype="stepfilled",alpha=0.5,color="grey",weights=[w_bkg]*n_bg,label="bg",zorder = -1)
+      mc_bg = fd(np.append(bg[idx].numpy(),mcdata[idx].numpy()))
+      mc_bg_w = np.append([w_bkg]*n_bg,total.numpy()*norm_int)
+    else:
+      mc_bg = fd(mcdata[idx].numpy())
+      mc_bg_w = total.numpy()*norm_int
     x_mc,y_mc = hist_line(mc_bg,mc_bg_w,bins,xrange)
     #ax.plot(x_mc,y_mc,label="total fit")
-    ax.hist(mc_bg,weights=mc_bg_w,bins=bins,range=xrange,histtype="step",label="total fit",zorder = 100)
+    ax.hist(mc_bg,weights=mc_bg_w,bins=bins,range=xrange,histtype="step",color=next(color),label="total fit",zorder = 100)
     for i in a_sig:
       weights = a_weight[i]
       x,y = hist_line(fd(mcdata[idx].numpy()),weights,bins,xrange,inter)
       y = y
-      ax.plot(x,y,label=res_name[i],linestyle="solid",linewidth=1)
+      ax.plot(x,y,label=res_name[i],linestyle="solid",linewidth=1,color=next(color))
     if legend:
-      ax.legend(framealpha=0.5)
+      ax.legend(framealpha=0.5,fontsize="small")
     ax.set_ylabel("events/({:.3f} {})".format((x_mc[1]-x_mc[0]),units))
     ax.set_xlabel("{} {}".format(display,units))
     if xrange is not None:
@@ -282,13 +305,15 @@ def plot(params_file="final_params.json",res_file="Resonances",res_list=None,pm_
   n = len(plot_list)
   if not os.path.exists("figure"):
     os.mkdir("figure")
+  #plt.style.use("classic") 
   for i in range(n):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
+    
     name = plot_list[i]
     plot_params(ax,name,**params_config[name])
     fig.savefig("figure/"+name+".pdf")
-    fig.savefig("figure/"+name+".png")
+    fig.savefig("figure/"+name+".png",dpi=300)
  
 
 
