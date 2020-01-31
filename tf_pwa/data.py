489e5ff2427aa0564b4600ae52999dd131361363
Yinrui Liu <liuyinrui16@mails.ucas.ac.cn>
2020-01-31 13:41:43 +0800

    extract gen_coef from amplitude.py to variable.py (primary verison)

--- tf_pwa/data.py
+++ tf_pwa/data.py
@@ -9,7 +9,8 @@ Data cache can be implemented based on the dynamic features of `list` and `dict`
 import numpy as np
 #import tensorflow as tf
 #from pysnooper import  snoop
-from .particle import BaseParticle, BaseDecay, DecayChain
+
+from .particle import BaseParticle, BaseDecay, DecayChain, DecayGroup
 from .angle_tf import LorentzVector, EularAngle
 from .tensorflow_wrapper import tf
 
@@ -118,59 +119,114 @@ def split_generator(data, batch_size, axis=0):
 def data_map(data, fun, args=(), kwargs=None):
     kwargs = kwargs if kwargs is not None else {}
     def g_fun(*args1, **kwargs1):
-        yield fun(*args1, **kwargs1)
+        return [fun(*args1, **kwargs1)]
     g = data_generator(data, fun=g_fun, args=args, kwargs=kwargs)
     return next(g)
 
+def data_merge(data1, data2, axis=0):
+  def flatten(data):
+    ret = []
+    def data_list(dat):
+      ret.append(dat)
+    data_map(data, data_list)
+    return ret
+  def rebuild(data, ret):
+    idx = -1
+    def data_build(_dat):
+      nonlocal idx
+      idx += 1
+      return ret[idx]
+    data = data_map(data, data_build)
+    return data
+  f_data1 = flatten(data1)
+  f_data2 = flatten(data2)
+  m_data = [tf.concat(data_i, axis=axis) for data_i in zip(f_data1, f_data2)]
+  return rebuild(data1, m_data)
+
+def data_shape(data, axis=0, all_list=False):
+  def flatten(dat):
+    ret = []
+    def data_list(dat1):
+      ret.append(dat1.shape)
+    data_map(dat, data_list)
+    return ret
+  shapes = flatten(data)
+  if all_list:
+    return shapes
+  return shapes[0][axis]
+
 def flatten_dict_data(data, fun="{}/{}".format):
-  if isinstance(data, dict):
+  def dict_gen(data):
+    return data.items()
+  def list_gen(data):
+    return enumerate(data)
+
+  if isinstance(data, (dict, list, tuple)):
     ret = {}
-    for i in data:
-      tmp = flatten_dict_data(data[i])
-      if isinstance(tmp, dict):
-        for j in tmp:
-          ret[fun(i, j)] = tmp[j]
+    gen_1 = dict_gen if isinstance(data, dict) else list_gen
+    for i, data_i in gen_1(data):
+      tmp = flatten_dict_data(data_i)
+      if isinstance(tmp, (dict, list, tuple)):
+        gen_2 = dict_gen if isinstance(tmp, dict) else list_gen
+        for j, tmp_j in gen_2(tmp):
+          ret[fun(i, j)] = tmp_j
       else:
         ret[i] = tmp
     return ret
   return data
 
-# data process
-def infer_momentum(p, decay_chain: DecayChain, center_mass=True) -> dict:
+def struct_momentum(p, center_mass=True) -> dict:
     """
-    {outs:momentum} => {top:{p:momentum},inner:{p:..},outs:{p:..}}
+    {outs:momentum} => {outs:{p:momentum}}
     """
-    p_outs = {}
+    ret = {}
     if center_mass:
         ps_top = []
-        for i in decay_chain.outs:
+        for i in p:
             ps_top.append(p[i])
         p_top = tf.reduce_sum(ps_top, 0)
-        for i in decay_chain.outs:
-            p_outs[i] = LorentzVector.rest_vector(p_top, p[i])
+        for i in p:
+            ret[i] = {"p": LorentzVector.rest_vector(p_top, p[i])}
     else:
-        for i in decay_chain.outs:
-            p_outs[i] = p[i]
+        for i in p:
+            ret[i] = {"p": p[i]}
+    return ret
 
+# data process
+def infer_momentum(data, decay_chain: DecayChain) -> dict:
+    """
+    {outs:{p:momentum}} => {top:{p:momentum},inner:{p:..},outs:{p:..}}
+    """
     st = decay_chain.sorted_table()
-    ret = {}
     for i in st:
+        if i in data:
+            continue
         ps = []
         for j in st[i]:
-            ps.append(p_outs[j])
-        ret[i] = {"p": tf.reduce_sum(ps, 0)}
-    return ret
+            ps.append(data[j]["p"])
+        data[i] = {"p": tf.reduce_sum(ps, 0)}
+    return data
 
 def add_mass(data: dict, _decay_chain: DecayChain = None) -> dict:
     """
     {top:{p:momentum},inner:{p:..},outs:{p:..}} => {top:{p:momentum,m:mass},...}
     """
     for i in data:
-        p = data[i]["p"]
-        data[i]["m"] = LorentzVector.M(p)
+        if isinstance(i, BaseParticle):
+            p = data[i]["p"]
+            data[i]["m"] = LorentzVector.M(p)
     return data
 
-def cal_angle(data: dict, decay_chain: DecayChain = None) -> dict:
+def add_weight(data: dict, weight: float = 1.0) -> dict:
+    """
+    {top:{p:momentum},inner:{p:..},outs:{p:..}} => {top:{p:momentum,m:mass},...}
+    """
+    data_size = data_shape(data)
+    weight = [1.0] * data_size
+    data["weight"] = np.array(weight)
+    return data
+
+def cal_helicity_angle(data: dict, decay_chain: DecayChain = None) -> dict:
     """
     {top:{p:momentum},inner:{p:..},outs:{p:..}} => {top:{p:momentum,m:mass},...}
     """
@@ -186,22 +242,57 @@ def cal_angle(data: dict, decay_chain: DecayChain = None) -> dict:
     set_x = {decay_chain.top: np.array([[1.0, 0.0, 0.0]])}
     set_z = {decay_chain.top: np.array([[0.0, 0.0, 1.0]])}
     set_decay = list(decay_chain)
-    while len(set_decay) > 0:
+    while set_decay:
         extra_decay = []
         for i in set_decay:
             if i.core in set_x:
                 for j in i.outs:
                     data[i][j] = {}
                     z2 = LorentzVector.vect(part_data[i]["rest_p"][j])
-                    ang, x = EularAngle.angle_zx_z_gety(set_z[i.core], set_x[i.core], z2)
+                    ang, x = EularAngle.angle_zx_z_getx(set_z[i.core], set_x[i.core], z2)
                     set_x[j] = x
                     set_z[j] = z2
                     data[i][j]["ang"] = ang
                     data[i][j]["x"] = x
+                    data[i][j]["z"] = z2
             else:
                 extra_decay.append(i)
         set_decay = extra_decay
     return data
+
+def cal_angle(data: list, decay_group: DecayGroup) -> dict:
+  for i in decay_group:
+    data = cal_helicity_angle(data, i)
+  decay_chain_struct = decay_group.topology_structure()
+  set_x = {}
+  # for a the top rest farme
+  for decay_chain in decay_chain_struct:
+    for decay in decay_chain:
+      if decay.core == decay_group.top:
+        for i in decay.outs:
+          if (i not in set_x) and (i in decay_group.outs):
+            set_x[i] = decay
+  # or the first chain
+  for i in decay_group.outs:
+    if i not in set_x:
+      decay_chain = next(iter(decay_chain_struct))
+      for decay in decay_chain:
+        for j in decay.outs:
+          if i == j:
+            set_x[i] = decay
+  for decay_chain in decay_group:
+    for decay in decay_chain:
+      for i in decay.outs:
+        if i in decay_group.outs:
+          if decay != set_x[i]:
+            x1 = data[decay][i]["x"]
+            x2 = data[set_x[i]][i]["x"]
+            z1 = data[decay][i]["z"]
+            z2 = data[set_x[i]][i]["z"]
+            ang = EularAngle.angle_zx_zx(z1, x1, z2, x2)
+            data[decay][i]["aligned_angle"] = ang
+  return data
+
 
 def Getp(M_0, M_1, M_2):
     M12S = M_1 + M_2
@@ -232,7 +323,7 @@ def get_relativate_momentum(data: dict, decay: BaseDecay, m0=None, m1=None, m2=N
     return p
 
 def test_process(fnames=None):
-    a, b, c, d, r = [BaseParticle(i) for i in ["A", "B", "C", "D", "R"]]
+    a, b, c, d = [BaseParticle(i) for i in ["A", "B", "C", "D"]]
     if fnames is None:
         p = {
             b: np.array([[1.0, 0.2, 0.3, 0.2]]),
@@ -241,14 +332,21 @@ def test_process(fnames=None):
         }
     else:
         p = load_dat_file(fnames, [b, c, d])
-    st = {b: [b], c: [c], d: [d], a: [b, c, d], r: [b, d]}
-    dec = DecayChain.from_sorted_table(st)
-    data = infer_momentum(p, dec)
-    data = add_mass(data, dec)
-    data = add_relativate_momentum(data, dec)
-    data = cal_angle(data, dec)
-    for i, j in enumerate(split_generator(data, 5000)):
-        print("split:", i, "is", j)
+    # st = {b: [b], c: [c], d: [d], a: [b, c, d], r: [b, d]}
+    decs = DecayChain.from_particles(a, [b, c, d])
+    data = struct_momentum(p)
+    for dec in decs:
+      data = infer_momentum(data, dec)
+      data = add_mass(data, dec)
+      data = add_relativate_momentum(data, dec)
+    data = cal_angle(data, DecayGroup(decs))
+    data = add_weight(data)
+    print(len(list(split_generator(data, 5000))))
     from pprint import pprint
-    pprint(data_map(data, lambda x: x.numpy()))
+    def to_numpy(data):
+      if hasattr(data, "numpy"):
+        return data.numpy()
+      return data
+    data = data_map(data, to_numpy)
+    pprint(data)
     return data
--- tf_pwa/variable.py
+++ tf_pwa/variable.py
@@ -1,65 +1,172 @@
 import tensorflow as tf
+from numpy import pi
+from .utils import is_complex
 
-
+'''
 def fix_value(x): # a dtype constant
   def f(shape=None,dtype=None):
     if dtype is not None:
       return tf.Variable(x,dtype=dtype)
     return x
   return f
-
 def rand_value(x): # random uniform [0,1]
   def f(shape=None,dtype=None):
     if dtype is not None:
       return tf.Variable(x*tf.random.uniform((),dtype=dtype),dtype=dtype)
     return x*tf.random.uniform((),dtype=dtype)
   return f
-
 def range_value(a,b): # random uniform [a,b]
   def f(shape=None,dtype=None):
     return (b-a)*tf.random.uniform((),dtype=dtype)+a
   return f
-
+'''
 
 class Vars(object):
-  def __init__(self, env):
-    self.env = env # 不过self.env.add_weight不通用化
+  def __init__(self, add_method):
+    self.add_method = add_method # keras.Model.add_weight 不够通用化
     self.variables = {}
 
-  def add(self,name,size=None,var=None,range=None,trainable=True,**args):
-    if name not in self.variables:
-      if var is None:
-        if size is None: # random [0,1]
-          self.variables[name] = self.env.add_weight(name,trainable=trainable,**args)
-        else:
-          if range is None: # random [0,size]
-            self.variables[name] = self.env.add_weight(name,initializer=rand_value(size),trainable=trainable,**args)
-          else: # random [a,b]
-            self.variables[name] = self.env.add_weight(name,initializer=range_value(*range),trainable=trainable,**args)
-      else: # fix value
-        self.variables[name] = self.env.add_weight(name,initializer=fix_value(var),trainable=trainable,**args)
-    return self.variables[name]
-  
-  __call__ = add
 
-  
+  def add_var(self,name,value=None,range_=None,trainable=True,**args):
+    if name not in self.variables: # a new var
+      if value is None:
+        if range_ is None:  
+          self.variables[name] = self.add_method(name,trainable=trainable,**args)# 如果没iniitializer那会是啥？
+        else: # random [a,b]
+          self.variables[name] = self.add_method(name,initializer=tf.initializers.RandomUniform(*range_),trainable=trainable,**args)
+      else: # constant value
+        self.variables[name] = self.add_method(name,initializer=tf.initializers.Constant(value),trainable=trainable,**args)
+    return self.variables[name]
+ 
+  __call__ = add_var
+
+
   def get(self,name):
     if name not in self.variables:
       raise Exception("{} not found".format(name))
     return self.variables[name]
   
-  def set(self,name,var):
+  def set(self,name,value):
     if name not in self.variables:
       raise Exception("{} not found".format(name))
-    return self.variables[name].assign(var)
+    return self.variables[name].assign(value)
 
 
-class Variable(object): #不用？
-  def __init__(self,name,var,err=0.1,range=(None,None)):
+class Variable(object): # fitting parameters for the amplitude model
+  def __init__(self,add_method,res,res_decay,polar,name=None,value=None,err=None,range_=(None,None)):
     self.name = name
-    self.var = var
+    self.value = value
     self.err = err
-    self.range_ = range
+    self.range_ = range_
+
+    self.add_var = Vars(add_method) # 通过Vars类来操作variables
+    self.res = res
+    self.polar = polar # r*e^{ip} or x+iy
+    self.res_decay = res_decay
+    self.coef = {}
+    self.coef_norm = {}
+
+  def params_mass_width(self): # add "mass" "width" fitting parameters
+    for i in self.res:
+      if "float" in self.res[i]: # variable
+        floating = self.res[i]["float"]
+        floating = str(floating)
+        if "m" in floating:
+          self.res[i]["m0"] = self.add_var(name=i+"_m",value = self.res[i]["m0"]) #然后self.res[i]["m0"]就成一个变量了（BW里会调用）
+        if "g" in floating:
+          self.res[i]["g0"] = self.add_var(name=i+"_g",value = self.res[i]["g0"])
+
+
+  def init_res_param(self):
+    self.params_mass_width()
+    const_first = True # 第一个共振态系数为1，除非Resonance.yml里指定了某个"total"
+    for i in self.res:
+      if "total" in self.res[i]:
+        const_first = False
+    res_tmp = [i for i in self.res]
+    res_all = [] # ensure D2_2460 in front of D2_2460p
+    # order for coef_head
+    while len(res_tmp) > 0:
+      i = res_tmp.pop()
+      if "coef_head" in self.res[i]: # e.g. "D2_2460" for D2_2460p
+        coef_head = self.res[i]["coef_head"]
+        if coef_head in res_tmp:
+          res_all.append(coef_head)
+          res_tmp.remove(coef_head)
+      res_all.append(i)
+    for i in res_all:
+      const_first = self.init_res_param_sig(i,self.res[i],const_first=const_first)
+    
+  def init_res_param_sig(self,head,config,const_first=False): #head名字，config参数
+    self.coef[head] = []
+    chain = config["Chain"]
+    coef_head = head
+    if "coef_head" in config:
+      coef_head = config["coef_head"] #这一步把D2_2460p参数变成D2_2460的了
+    if "total" in config:
+      N_tot = config["total"]
+      if is_complex(N_tot):
+        N_tot = complex(N_tot)
+        rho,phi = N_tot.real,N_tot.imag
+      else:
+        rho,phi = N_tot #其他类型的complex. raise error?
+      r = self.add_var(name=coef_head+"r",value=rho,trainable=False)
+      i = self.add_var(name=head+"i",value=phi,trainable=False)
+    elif const_first:#先判断有么有total，否则就用const_first
+      r = self.add_var(name=coef_head+"r",value=1.0,trainable=False)
+      i = self.add_var(name=head+"i",value=0.0,trainable=False)
+    else:
+      r = self.add_var(name=coef_head+"r",range_=(0,2.0))
+      i = self.add_var(name=head+"i",range_=(-pi,pi))
+    self.coef_norm[head] = [r,i]
+    if "const" in config: # H里哪一个参数设为常数1
+      const = list(config["const"])
+    else:
+      const = [0,0]
+    ls,arg = self.gen_coef(head,0,coef_head+"_",const[0])
+    self.coef[head].append(arg)
+    ls,arg = self.gen_coef(head,1,coef_head+"_d_",const[1])
+    self.coef[head].append(arg)
+    return False # const_first
+    
+  def gen_coef(self,idx,layer,coef_head,const = 0) :
+    if const is None:
+      const = 0 # set the first to be constant 1 by default
+    if isinstance(const,int):
+      const = [const] # int2list, in case more than one constant
+    ls = self.res_decay[idx][layer].get_ls_list() # allowed l-s pairs
+    n_ls = len(ls)
+    const_list = []
+    for i in const:
+      if i<0:
+        const_list.append(n_ls + i) # then -1 means the last one
+      else:
+        const_list.append(i)
+    arg_list = []
+    for i in range(n_ls):
+      l,s = ls[i]
+      name = "{head}BLS_{l}_{s}".format(head=coef_head,l=l,s=s)
+      if i in const_list:
+        tmp_r = self.add_var(name=name+"r",value=1.0,trainable=False)
+        tmp_i = self.add_var(name=name+"i",value=0.0,trainable=False)
+        arg_list.append((name+"r",name+"i"))
+      else :
+        if self.polar:
+          tmp_r = self.add_var(name=name+"r",range_=(0,2.0))
+          tmp_i = self.add_var(name=name+"i",range_=(-pi,pi))
+        else:
+          tmp_r = self.add_var(name=name+"r",range_=(-1,1))
+          tmp_i = self.add_var(name=name+"i",range_=(-1,1))
+        arg_list.append((name+"r",name+"i"))
+    return ls,arg_list
+
+
+  def get(self,name):
+    return self.add_var.get(name)
+
+  def set(self,name,value):
+    return self.add_var.set(name,value)
+
   
   def set_range(self,range_):
     self.range_ = range_
