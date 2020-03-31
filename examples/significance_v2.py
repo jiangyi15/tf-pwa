672862410a7fe175defb7b525e28f2c5f340f6f7
jiangy <jiangyi15@mails.ucas.ac.cn>
2020-03-31 22:18:32 +0800

    add nlls and ndf print

--- examples/significance_v2.py
+++ examples/significance_v2.py
@@ -24,7 +24,7 @@ def single_fit(config_dict):
     return fit_result.min_nll, fit_result.ndf
 
 
-def multi_fit(config, num=1):
+def multi_fit(config, num=5):
     nll, ndf = single_fit(config)
     for i in range(num):
         nll_i, ndf_i = single_fit(config)
@@ -55,21 +55,27 @@ def cal_significance(config_name, res):
     with open(config_name) as f:
         config = yaml.safe_load(f)
     nll, ndf = single_fit(config)
+    nlls = {"base": nll}
+    ndfs = {"base": ndf}
     print("nll: {}, ndf: {}".format(nll, ndf))
     signi = {}
     for i in res:
         print("#veto {}".format(i))
         config_i = veto_resonance(config, i)
         nll_i, ndf_i = single_fit(config_i)
+        nlls[i] = nll_i
+        ndfs[i] = ndf_i
         print("nll: {}, ndf: {}".format(nll_i, ndf_i))
         signi[i] = significance(nll, nll_i, abs(ndf - ndf_i))
-    return signi
+    return signi, nlls, ndfs
 
 
 def main():
-    res = ["X(3940)(1+)", "Psi(4660)", "Psi(4230)", "Psi(4390)", "Psi(4260)", "Psi(4360)"]
-    signi = cal_significance("config.yml", res)
-    pprint(signi)
+    res = ["X(3940)(1+)", "Psi(4660)", "Psi(4230)", "Psi(4390)", "Psi(4260)", "Psi(4360)", "Zc(3900)","Psi(4160)", "Psi(4415)","Psi(4040)"]
+    signi, nlls, ndfs = cal_significance("config.yml", res)
+    print("base", nlls["base"], ndfs["base"])
+    for i in signi:
+        print(i, signi[i], nlls[i], ndfs[i])
 
 
 if __name__ == "__main__":
