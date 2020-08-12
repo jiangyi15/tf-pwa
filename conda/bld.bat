d6196dbbc63f2dd2e1050daee28ea62a3c29936f
jiangy <jiangyi15@mails.ucas.ac.cn>
2020-08-12 09:43:56 +0800

    using  for conda build

--- conda/bld.bat
+++
@@ -1,8 +0,0 @@
-"%PYTHON%" -m pip install . --no-deps
-if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
