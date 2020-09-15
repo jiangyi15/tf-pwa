0af8cc822972f3dcafbdf453aad0b7532ba214cc
jiangy <jiangyi15@mails.ucas.ac.cn>
2020-09-15 10:05:35 +0800

    mv test in variable

--- tf_pwa/variable.py
+++ tf_pwa/variable.py
@@ -1180,19 +1180,3 @@ class Variable(object):
 
         # return tf.stack(var_list)
         return var_list
-
-
-if __name__ == "__main__":
-    m = Variable("R_m", value=2.1)  # trainable is True by default
-    g_ls = Variable("A2BR_H", shape=[3], cplx=True)
-    fcr = Variable("R_total", cplx=True)
-    m1 = Variable("R1_m", value=2.3)
-    g_ls1 = Variable("A2BR1_H", shape=[3], cplx=True)
-    fcr1 = Variable("R1_total", cplx=True)
-
-    print(g_ls.value)
-    print(g_ls())
-
-    m.fixed(2.4)
-    g_ls.sameas(g_ls1)
-    fcr.r_shareto(fcr1)
