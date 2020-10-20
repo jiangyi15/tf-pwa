da4c0229f21535b76f77b3f1cd449039d7fad412
jiangy <jiangyi15@mails.ucas.ac.cn>
2020-10-20 20:47:43 +0800

    add jinja2_load.py

---
+++ tutorials/examples/jinja2_load.py
@@ -0,0 +1,33 @@
+import argparse
+from jinja2 import Template
+import os
+
+
+def load_config(input_file, variable_file):
+    """
+    read input_file and render it with variable file
+    """
+    with open(input_file) as f:
+        template = Template(f.read())
+
+    variable = os.environ.copy()
+    if args.variable != "":
+        with open(variable_file) as f:
+            exec(f.read(), variable)
+
+    ret = template.render(**variable)
+    return ret
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Jinja")
+    parser.add_argument("input_file")
+    parser.add_argument("variable", nargs="?", default="")
+    args = parser.parse_args()
+
+    s = load_config(args.input_file, args.variable)
+    print(s)
+
+
+if __name__ == "__main__":
+    main()
