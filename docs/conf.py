eb59ddfdc40e0001f8c278ba870087dc74ae0c3c
Remco de Boer <redeboer@gmx.com>
2020-10-24 12:57:14 +0200

    docs: simplify conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,67 +1,44 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""Configuration file for the Sphinx documentation builder.
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
+This file only contains a selection of the most common options. For a full
+list see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
 
 import os
-import sys
 
-import tf_pwa
 from tf_pwa.amp import PARTICLE_MODEL, get_config
-from tf_pwa.experimental import extra_amp
-
-sys.path.insert(0, os.path.abspath(".."))
-
 
 # -- Project information -----------------------------------------------------
-
 project = "TFPWA"
-copyright = "2020, "
-author = ""
-
+copyright = "2020, Yi Jiang"
+author = "Yi Jiang"
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.mathjax",
     "sphinx.ext.doctest",
     "sphinx.ext.graphviz",
 ]
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
-
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
+source_suffix = [
+    ".rst",
+]
 
 # -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
+html_title = "TFPWA"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
-
-
-autodoc_mock_imports = ["tensorflow", "iminuit"]
+# -- Options for API ---------------------------------------------------------
+autodoc_mock_imports = [
+    "iminuit",
+    "tensorflow",
+]
 
 
 def add_indent(s, number=2):
