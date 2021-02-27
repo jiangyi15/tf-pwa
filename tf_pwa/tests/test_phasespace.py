e0d1be7ffc1a59bf7ece160a75451f159b3e58b8
jiangy <jiangyi15@mails.ucas.ac.cn>
2021-02-27 13:37:21 +0800

    feat: general phsp

--- tf_pwa/tests/test_phasespace.py
+++ tf_pwa/tests/test_phasespace.py
@@ -16,3 +16,9 @@ def test_phasespace():
     p_all = data[0] + data[1] + data[2]
     assert np.allclose(LorentzVector.M(p_all), [10] * 100)
     assert np.allclose(p_all[:, 1:3], np.zeros_like(p_all[:, 1:3]))
+
+
+def test_generate_phsp():
+    (a, b), c = generate_phsp(5.0, ((3.0, (1.0, 1.0)), 1.0))
+    assert np.allclose(LorentzVector.M(a + b + c), 5.0)
+    # assert np.sum(np.abs((a+b+c)[:,1:])) < 2e-5
