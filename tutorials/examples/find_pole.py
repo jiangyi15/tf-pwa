1d08edc59e6537d831c99c2cc0d2916b74f2e633
jiangyi15 <jiangyi15@mails.ucas.ac.cn>
2023-01-07 21:50:36 +0800

    feat: solve pole examples

--- tutorials/examples/find_pole.py
+++ tutorials/examples/find_pole.py
@@ -1,24 +1,64 @@
+import extra_amp
 import numpy as np
-import sympy
 import tensorflow as tf
 
-from tf_pwa.formula import BWR_dom, create_complex_root_sympy_tfop
-from tf_pwa.variable import Variable, VarsManager
+from tf_pwa.config_loader import MultiConfig
+from tf_pwa.data import data_to_numpy
+from tf_pwa.utils import error_print
 
-m, m0, g0 = sympy.var("m m0 g0")
+config = MultiConfig(["config.yml", "config_c.yml"], total_same=True)
+config.set_params("final_params.json")
+config.inv_he = np.load("error_matrix.npy")
 
+ret = {}
+with config.params_trans() as pm:
+    for i in config.configs[1].get_decay():
+        if "NR" in str(i):
+            continue
+        res = i[1].core
+        p = res.solve_pole()
+        ret[res] = [
+            res.get_mass() * 1000,
+            res.get_width() * 1000,
+            tf.math.real(p) * 1000,
+            tf.math.imag(p) * 1000,
+        ]
 
-f = BWR_dom(m, m0, g0, 1, 0.3, 0.3)
+a, b = data_to_numpy((ret, pm.get_error(ret)))
 
-g = create_complex_root_sympy_tfop(f, [m0, g0], m, 1.0 + 0.01j)
+print("res  ", "\t", "pole(mass - iwidth/2)", "\t", "BW mass/(width/2)")
+for i in a:
+    if b[i][3] == 0:
+        continue
+    print(
+        i,
+        "\t",
+        error_print(a[i][2], b[i][2]),
+        "\t",
+        error_print(a[i][0], b[i][0]),
+    )
+    print(
+        i,
+        "\t ",
+        error_print(a[i][3], b[i][3]),
+        "\t",
+        error_print(a[i][1] / 2, b[i][1] / 2),
+    )
 
-
-vm = VarsManager()
-a = Variable("a", value=1.0, vm=vm)
-b = Variable("b", value=0.1, vm=vm)
-
-with vm.error_trans(np.array([[0.010, 0.0], [0.0, 0.001]])) as pm:
-    c = g(a(), b())
-    d = tf.math.real(c)
-    e = tf.math.imag(c)
-print(c, pm.get_error([d, e]))
+for i in a:
+    if b[i][3] != 0:
+        continue
+    print(
+        i,
+        "\t",
+        error_print(a[i][2], b[i][2]),
+        "\t",
+        error_print(a[i][0], b[i][0]),
+    )
+    print(
+        i,
+        "\t ",
+        error_print(a[i][3], b[i][3]),
+        "\t",
+        error_print(a[i][1] / 2, b[i][1] / 2),
+    )
