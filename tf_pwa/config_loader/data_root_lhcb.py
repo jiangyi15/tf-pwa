ef8b23ffd14a0f7a054f4bc37ae681edadf8f650
Jiang Yi (蒋艺) <jiangyi15@mails.ucas.ac.cn>
2023-08-23 16:03:34 +0800

    ci: fix error

--- tf_pwa/config_loader/data_root_lhcb.py
+++ tf_pwa/config_loader/data_root_lhcb.py
@@ -63,7 +63,7 @@ class RootData(MultiData):
             print("uproot < 4 is not support")
             return [None]
         if idx not in self.dic:
-            return [None]
+            return None
         p4 = self.get_p4(idx)
         n_data = [i.shape[0] for i in p4]
         p4 = [list(np.moveaxis(i, 1, 0)) for i in p4]
