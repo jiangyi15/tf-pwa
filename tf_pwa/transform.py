ca56654e5923665c0b05e0b16c3ebe6a937d8b95
jiangyi15 <jiangyi15@mails.ucas.ac.cn>
2023-09-15 00:11:27 +0800

    fixed: ci error

---
+++ tf_pwa/transform.py
@@ -0,0 +1,39 @@
+from .config import create_config
+
+set_trans, get_trans, register_trans = create_config()
+
+T = "Tensor"
+
+
+class BaseTransform:
+    def __call__(self, x: T) -> T:
+        return self.call(x)
+
+    def call(self, x: T) -> T:
+        raise NotImplementedError()
+
+    def inverse(self, y: T) -> T:
+        raise NotImplementedError()
+
+
+def create_trans(item: dict) -> BaseTransform:
+    cls = get_trans(item.get("model", "default"))
+    obj = cls(**item)
+    return obj
+
+
+@register_trans("default")
+@register_trans("linear")
+class LinearTrans(BaseTransform):
+    def __init__(
+        self, k: float = 1.0, b: float = 0.0, model: str = "", **kwargs
+    ):
+        self.k = k
+        self.b = b
+        self.model = model
+
+    def call(self, x: T) -> T:
+        return self.k * x + self.b
+
+    def inverse(self, x: T) -> T:
+        return (x - self.b) / self.k
