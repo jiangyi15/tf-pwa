d2866c194d7903508612c6eaa023b3078f04d4b0
jiangyi15 <jiangyi15@mails.ucas.ac.cn>
2023-10-09 23:44:00 +0800

    feat: add flatte2 model

--- tf_pwa/amp/flatte.py
+++ tf_pwa/amp/flatte.py
@@ -35,13 +35,14 @@ Required input arguments `mass_list: [[m11, m12], [m21, m22]]` for :math:`m_{i,1
 
     """
 
-    def __init__(self, *args, mass_list=None, **kwargs):
+    def __init__(self, *args, mass_list=None, im_sign=1, **kwargs):
         super().__init__(*args, **kwargs)
         if mass_list is None:
             raise ValueError("required mass_list: [[a, b], [mc, md]]")
         self.mass_list = mass_list
         self.g_value = []
         self.float_list = list(kwargs.get("float", []))
+        self.im_sign = im_sign
 
     def init_params(self):
         self.d = 3.0
@@ -95,5 +96,30 @@ Required input arguments `mass_list: [[m11, m12], [m21, m22]]` for :math:`m_{i,1
         re = delta_s + tf.math.real(rho)
         im = tf.math.imag(rho)
         d = re * re + im * im
-        ret = tf.complex(re / d, -im / d)
+        ret = tf.complex(re / d, -self.im_sign * im / d)
         return ret
+
+
+@register_particle("FlatteC")
+class ParticleFlatte2(ParticleFlatte):
+    """
+
+    Flatte like formula
+
+.. math::
+
+    R(m) = \\frac{1}{m_0^2 - m^2 - m_0 (\\sum_{i}  g_i \\frac{q_i}{m})}
+
+.. math::
+
+    q_i = \\begin{cases}
+    \\frac{\\sqrt{(m^2-(m_{i,1}+m_{i,2})^2)(m^2-(m_{i,1}-m_{i,2})^2)}}{2m} & (m^2-(m_{i,1}+m_{i,2})^2)(m^2-(m_{i,1}-m_{i,2})^2) >= 0 \\\\
+    \\frac{i\\sqrt{|(m^2-(m_{i,1}+m_{i,2})^2)(m^2-(m_{i,1}-m_{i,2})^2)|}}{2m} & (m^2-(m_{i,1}+m_{i,2})^2)(m^2-(m_{i,1}-m_{i,2})^2) < 0 \\\\
+    \\end{cases}
+
+Required input arguments `mass_list: [[m11, m12], [m21, m22]]` for :math:`m_{i,1}, m_{i,2}`.
+
+    """
+
+    def __init__(self, *args, im_sign=-1, **kwargs):
+        super().__init__(self, *args, im_sign=im_sign, **kwargs)
