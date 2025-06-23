de847889ad2c840911c32a2e9a3c7310e3501cf3
Jonas Eschle <mayou36@jonas.eschle.com>
2025-06-23 08:33:10 +0200

    Reshape nll to scalar

--- tf_pwa/model/model.py
+++ tf_pwa/model/model.py
@@ -1261,7 +1261,7 @@ class FCN(object):
         constr = self.gauss_constr.get_constrain_term()
         constr_grad = self.gauss_constr.get_constrain_grad()
         self.cached_nll = nll + constr
-        return self.cached_nll, g + constr_grad
+        return tf.reshape(self.cached_nll, ()), g + constr_grad
 
     def get_nll_grad_hessian(self, x={}, batch=None):
         """
