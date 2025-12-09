refactor: fixed barrier_factor_norm when no bprime reduce nan in calculation

tf_pwa/amp/core.py
@@ -1079,14 +1079,22 @@ class HelicityDecay(AmpDecay):
             if self.has_bprime:
                 bp = Bprime_q2(l, q2, q02, d)
                 if self.has_ql:
-                    tmp = q2 ** (l / 2) * tf.cast(bp, dtype=q2.dtype)
+                    if l % 2 == 1:
+                        tmp = tf.abs(q2) ** (l / 2) * tf.cast(
+                            bp, dtype=q2.dtype
+                        )
+                    else:
+                        tmp = q2 ** (l / 2) * tf.cast(bp, dtype=q2.dtype)
+                    if self.barrier_factor_norm:
+                        tmp = tmp / tf.cast(tf.abs(q02), tmp.dtype) ** (l / 2)
                 else:
                     tmp = tf.ones_like(q2) * tf.cast(bp, dtype=q2.dtype)
-                if self.barrier_factor_norm:
-                    tmp = tmp / tf.cast(tf.abs(q02), tmp.dtype) ** (l / 2)
+
             else:
                 if self.has_ql:
                     tmp = q2 ** (l / 2)
+                    if self.barrier_factor_norm:
+                        tmp = tmp / tf.cast(tf.abs(q02), tmp.dtype) ** (l / 2)
                 else:
                     tmp = tf.ones_like(q2)
             if self.add_covariant_term:
@@ -1462,7 +1470,8 @@ class DecayChain(AmpDecayChain):
         # print(idx_s)#, amp_d)
         try:
             ret = einsum(idx_s, *amp_d)
-        except:
+        except Exception as e:
+            print(e)
             ret = tf.einsum(idx_s, *amp_d)
         # print(self, ret[0])
         # exit()

tf_pwa/angle.py
@@ -52,7 +52,7 @@ class Vector3(tf.Tensor):
         """
         The unit vector of itself. It has interface to *tf.linalg.normalize()*.
         """
-        p, _n = tf.linalg.normalize(self, axis=-1)
+        p = tf.math.l2_normalize(self, axis=-1)
         return p
 
     def cross_unit(self, other):